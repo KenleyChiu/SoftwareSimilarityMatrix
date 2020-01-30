8d900218a9951ea4d00e36e8f30cfd1979767a95
Keyaru <kenley_chiu@dlsu.edu.ph>
2020-01-30 11:09:16 +0800

    New Similarity

--- src/Backend/Similarity.java
+++ src/Backend/Similarity.java
@@ -9,37 +9,46 @@ public class Similarity {
     private Scanner prog1Scan, prog2Scan;
     private String filename1, filename2;
 
-    public String ReadCodeLine(String comparison) {
+    public void ReadCodeLine() {
         float sameLines = 0, totalLines = 0, lineLength = 0;
         StringBuilder prog1String = new StringBuilder();
         StringBuilder prog2String = new StringBuilder();
 
+        while(true)
+        {
+            if (prog1Scan.hasNext() && prog2Scan.hasNext()) {
+                String prog1Word = prog1Scan.nextLine();
+                String prog2Word = prog2Scan.nextLine();
 
-        while (prog1Scan.hasNextLine() || prog2Scan.hasNextLine()) {
-            String prog1Word = prog1Scan.nextLine();
-            String prog2Word = prog2Scan.nextLine();
+                prog1String.append(prog1Word + "\n");
+                prog2String.append(prog2Word + "\n");
 
-            prog1String.append(prog1Word + "\n");
-            prog2String.append(prog2Word + "\n");
+                if (prog1Word.equals(prog2Word)) {
+                    int newLineLength = prog1Word.length();
 
-            if (prog1Word.equals(prog2Word)) {
-                int newLineLength = prog1Word.length();
-
-                if (newLineLength > lineLength) {
-                    lineLength = newLineLength;
+                    if (newLineLength > lineLength) {
+                        lineLength = newLineLength;
+                    }
+                    sameLines++;
                 }
-                sameLines++;
+                totalLines++;
             }
-
-            if (!prog1Scan.hasNextLine() || !prog2Scan.hasNextLine()) break;
-            totalLines++;
+            else if(prog1Scan.hasNext())
+            {
+               totalLines++;
+            }
+            else if(prog2Scan.hasNext())
+            {
+                totalLines++;
+            }
+            else {
+                break;
+            }
         }
-        percentage = (sameLines / totalLines) * 100;
-
-        return filename1+" and "+filename2+" have a similarity rate of "+percentage+ "% when compared per " + comparison + ".";
+        percentage = (sameLines / totalLines);
     }
 
-    public String ReadCodeCharacter(String comparison) {
+    public void ReadCodeCharacter() {
         int countChar = 0;
         int countTotal = 0;
         while (true) {
@@ -76,21 +85,31 @@ public class Similarity {
         }
         prog1Scan.close();
         prog2Scan.close();
-        percentage = ((float) countChar / (float) countTotal) * (float) 100;
-        return filename1+" and "+filename2+" have a similarity rate of "+percentage+ "% when compared per " + comparison + ".";
+        percentage = ((float) countChar / (float) countTotal);
 
 
     }
 
-    public void readFile(String filename1, String filename2) throws FileNotFoundException {
-        File prog1File = new File("Codes/"+filename1);
-        File prog2File = new File("Codes/"+filename2);
+    public void readFile(String comparison) throws FileNotFoundException {
+        File prog1File = new File("Codes");
+        File prog2File = new File("Codes");
+        File[] file1 =prog1File.listFiles();
+        File[] file2 = prog2File.listFiles();
+        for(int i=0; i<file1.length; i++)
+        {
+            this.filename1=file1[i].getName();
+            for(int j=0; j<file2.length; j++)
+            {
+                this.filename2=file2[j].getName();
+                prog1Scan = new Scanner(prog1File);
+                prog2Scan = new Scanner(prog2File);
+                if(comparison.equals("line")) ReadCodeLine();
+                else ReadCodeCharacter();
+            }
+
+        }
         //File prog1File = new File("D:\\Downloads\\College stuff\\Term 5\\LBYCP2D\\SoftwareSimilarityGui\\src\\sample\\" + filename1);
         //File prog2File = new File("D:\\Downloads\\College stuff\\Term 5\\LBYCP2D\\SoftwareSimilarityGui\\src\\sample\\" + filename2);
-        this.filename1=filename1;
-        this.filename2=filename2;
-        prog1Scan = new Scanner(prog1File);
-        prog2Scan = new Scanner(prog2File);
     }
 
 }
