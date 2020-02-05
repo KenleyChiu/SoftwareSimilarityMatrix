9ec9cfda7bd2a02b2e5cb284e1588fa44ceab2e9
matthewsabu <matthew_sabularse@dlsu.edu.ph>
2020-02-06 05:33:12 +0800

    Matrix Displays Properly

--- src/Controller/StatusController.java
+++ src/Controller/StatusController.java
@@ -11,7 +11,9 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class StatusController implements Initializable {
@@ -23,6 +25,11 @@ public class StatusController implements Initializable {
     public Button check,quit;
     private Similarity compare = new Similarity();
     private String comparison = "line";
+    private DataObject dataObj;
+    private Boolean status = true;
+    private ArrayList<DataObject> objArray;
+    private ArrayList<ArrayList<DataObject>> objMatrix = new ArrayList<>();
+
 
 
     public void line() {
@@ -35,8 +42,18 @@ public class StatusController implements Initializable {
 
     public void createMatrix() throws FileNotFoundException {
 
-        if(compare.getMatrix() != null){
-            gridPane.getChildren().removeAll(); //not working
+
+
+        if(!status){
+//            gridPane.getChildren().removeAll(); //not working
+
+//            for(int x=0;x<35;x++){
+//                for(int y=0;y<35;y++){
+//                    gridPane.getChildren().remove(objMatrix.get(x).get(y).getRect());
+//                    System.out.println(objArray.get(0).getData());
+//                }
+//            }
+
         }
 
 //        if(lineChoice.isSelected()) line();
@@ -45,13 +62,25 @@ public class StatusController implements Initializable {
 //            type = 1;
 //        }
 
-        compare.readFile(comparison);
+        compare.readFile(comparison,status);
 
-        for(int x=0;x<compare.getMatrix().arraySize();x++){
+        objArray = new ArrayList<>();
+
+        if(status) first();
+        else second();
+
+
+
+    }
+
+    public void first(){
+        for(int x=0;x<compare.getMatrix1().arraySize();x++){
             //System.out.print(form.getMatrix().get(0).get(x) + " ");  //to see first row values
-            for(int y=0;y<compare.getMatrix().matrixSize();y++){
-                System.out.print(compare.getMatrix().getMatrix().get(y).get(x) + "   ");
-                DataObject dataObj = new DataObject(compare.getMatrix().getMatrix().get(y).get(x));
+            for(int y=0;y<compare.getMatrix1().matrixSize();y++){
+                System.out.print(compare.getMatrix1().getMatrix().get(y).get(x) + "   ");
+                dataObj = new DataObject(compare.getMatrix1().getMatrix().get(y).get(x));
+
+                //objArray.add(dataObj);
 
                 VBox vbox = new VBox();
                 vbox.setAlignment(Pos.CENTER);
@@ -65,10 +94,37 @@ public class StatusController implements Initializable {
                 //gridPane.setGridLinesVisible(true);
             }
             System.out.println();
+
+            //objMatrix.add(objArray);
+            status = false;
         }
+    }
 
+    public void second(){
+        for(int x=0;x<compare.getMatrix2().arraySize();x++){
+            //System.out.print(form.getMatrix().get(0).get(x) + " ");  //to see first row values
+            for(int y=0;y<compare.getMatrix2().matrixSize();y++){
+                System.out.print(compare.getMatrix2().getMatrix().get(y).get(x) + "   ");
+                dataObj = new DataObject(compare.getMatrix2().getMatrix().get(y).get(x));
 
+                //objArray.add(dataObj);
 
+                VBox vbox = new VBox();
+                vbox.setAlignment(Pos.CENTER);
+                vbox.getChildren().addAll(dataObj.getLabel());
+
+                StackPane pane = new StackPane();
+                pane.setAlignment(Pos.CENTER);
+                pane.getChildren().addAll(dataObj.getRect(),vbox);
+
+                gridPane.add(pane,y,x);
+                //gridPane.setGridLinesVisible(true);
+            }
+            System.out.println();
+
+            //objMatrix.add(objArray);
+            status = false;
+        }
     }
 
     public void toExit()
