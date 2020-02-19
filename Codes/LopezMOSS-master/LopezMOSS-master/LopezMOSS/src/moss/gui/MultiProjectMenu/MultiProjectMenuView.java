24e01608dc68674e742e47a984ef607243e3261e
Keyaru <kenley_chiu@dlsu.edu.ph>
2020-02-20 01:34:28 +0800

    Delete content of Codes

--- Codes/LopezMOSS-master/LopezMOSS-master/LopezMOSS/src/moss/gui/MultiProjectMenu/MultiProjectMenuView.java
+++
@@ -1,9 +0,0 @@
-package moss.gui.MultiProjectMenu;
-
-import com.airhacks.afterburner.views.FXMLView;
-
-/**
- * View for the multi-project menu
- */
-public class MultiProjectMenuView extends FXMLView {
-}
--- Codes/LopezMOSS-master/LopezMOSS-master/uml/boUML/142021.diagram
+++
@@ -1,3 +0,0 @@
-format 222
-
-end
