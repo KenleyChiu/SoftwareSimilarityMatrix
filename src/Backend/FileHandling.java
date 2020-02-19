d68cb6bc7973973627a96693f20104048946ebfd
Keyaru <kenley_chiu@dlsu.edu.ph>
2020-02-19 21:31:40 +0800

    Added for File handling methods

---
+++ src/Backend/FileHandling.java
@@ -0,0 +1,88 @@
+package Backend;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileHandling {
+
+    private int fileFilter=0;
+    private ArrayList<String> type= new ArrayList<>();
+
+    public void deleteFilesMerge(File mergeFile) {
+        String[]entries = mergeFile.list();
+        if (entries != null) {
+            for(String s: entries){
+                File currentFile = new File(mergeFile.getPath(),s);
+                currentFile.delete();
+            }
+        }
+    }
+
+    public void getAllFiles(File[] dir, Matrix data) throws IOException //get all files recursively
+    {
+        for (File files : dir) {
+            if (files.isDirectory())// if the list found is a directory
+            {
+                File[] newDir = files.listFiles(); //gets the filename of the files inside the directory
+                getFilesRecursively(newDir, files.getName(), data);// goes to the file method to recursively merge all of the codes
+                fileFilter = 0;
+            }
+        }
+    }
+
+    private void getFilesRecursively(File[] dir,String fileName, Matrix data) throws IOException {
+        for(File files: dir)
+        {
+            if(files.isDirectory())// if the list found is a directory
+            {
+                File[] newDir=files.listFiles(); //gets the filename of the files inside the directory
+                getFilesRecursively(newDir,fileName,data);// going to the method that will check every file recursively
+            }
+            else
+            {
+                if(filtering(files)) // checks whether the file is a .java or .ccp
+                {
+                    FileWriter fstream = new FileWriter("MergedCodes/"+fileName+".txt",true);
+                    //To get the file names of the users that satisfied the filter option
+                    if(fileFilter ==0)
+                    {
+                        data.addUser(fileName);//add the names for the GUI
+                        fileFilter =1;
+                    }
+                    //writes the files into a text file for comparison
+                    BufferedWriter writing = new BufferedWriter(fstream);
+                    Scanner input = new Scanner(files);
+                    while(input.hasNext())
+                    {
+                        writing.write(input.nextLine());
+                        writing.newLine();
+                    }
+                    writing.close();
+                }
+
+            }
+        }
+    }
+
+    //Filters out the files that are not included in the checkbox
+    private boolean filtering(File files)
+    {
+        for (String s : type) {
+            if (files.getName().contains(s)) return true;
+        }
+        return false;
+    }
+
+
+    public void setFilter(ArrayList<String> type) {
+        this.type=type;
+    }
+
+
+
+
+}
