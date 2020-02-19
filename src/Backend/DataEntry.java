3262e84d18f18b32976c497016ef5f2373b01ed8
matthewsabu <matthew_sabularse@dlsu.edu.ph>
2020-02-19 09:29:02 +0800

    object for tableview

--- src/Backend/DataEntry.java
+++ src/Backend/DataEntry.java
@@ -1,12 +1,41 @@
 package Backend;
 
 
-public class TopSimilar {
+import javafx.beans.property.SimpleStringProperty;
 
-    private SimpleStringProperty progName1;
+public class DataEntry {
 
-    public void setProgName1(String name){
-        progName1Property().
+    private final SimpleStringProperty progName1,progName2,percentage;
+//    private double percentage;
+
+
+    public DataEntry(String name1,String name2,String score){
+        this.progName1= new SimpleStringProperty(name1);
+        this.progName2=new SimpleStringProperty(name2);
+        this.percentage=new SimpleStringProperty(score);
     }
 
+    public String getN1(){
+        return progName1.get();
+    }
+
+    public String getN2(){
+        return progName2.get();
+    }
+
+    public String getP(){
+        return percentage.get();
+    }
+
+    public void setN1(String n){
+        progName1.set(n);
+    }
+
+    public void setN2(String n){
+        progName2.set(n);
+    }
+
+    public void setP(String n){
+        percentage.set(n);
+    }
 }
