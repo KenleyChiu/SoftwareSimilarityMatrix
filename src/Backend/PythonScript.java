4fd7453eccf59acedda168e47ce1b7e240c72a61
Keyaru <kenley_chiu@dlsu.edu.ph>
2020-02-22 03:30:39 +0800

    Added to change file path automatically

--- src/Backend/PythonScript.java
+++ src/Backend/PythonScript.java
@@ -3,38 +3,38 @@ package Backend;
 import java.io.*;
 
 public class PythonScript {
-    public static void main(String a[]) {
+    public void changeScript() {
         try {
-//
-//                String prg = "import os\n" +
-//                        "from git.repo.base import Repo\n" +
-//                        "from datetime import datetime\n" +
-//                        "\n" +
-//                        "\n" +
-//                        "# create path name\n" +
-//                        "path = \"Codes\" \n" +
-//                        "\n" +
-//                        "# create dataset dir\n" +
-//                        "if not os.path.exists(path):\n" +
-//                        "    os.makedirs(path)\n" +
-//                        "\n" +
-//                        "# go to dir\n" +
-//                        "os.chdir(path)\n" +
-//                        "\n" +
-//                        "# acquire repositories\n" +
-//                        "with open(\"D:\\\\Downloads\\\\College stuff\\\\SoftwareSimilarityMatrix\\\\repo.txt\",'r') as repo_list:\n" +
-//                        "    for count, repo in enumerate(repo_list):    \n" +
-//                        "       pair_id = \"LBYCPD2_Pair\" + str(count)\n" +
-//                        "       print(\"Downloading \", pair_id)\n" +
-//                        "       Repo.clone_from(repo.strip(), pair_id, branch='master')\n" +
-//                        "   \n" +
-//                        "print(\"---- COMPLETED ----\")";
-//                BufferedWriter out = new BufferedWriter(new FileWriter("RepoGrabber.py"));
-//                out.write(prg);
-//                out.close();
-            Process p = Runtime.getRuntime().exec("python RepoGrabber.py");
-            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            System.out.println("Completed:" + in.readLine());
+            File repo= new File("Repo.txt");
+                String prg = "import os\n" +
+                        "from git.repo.base import Repo\n" +
+                        "from datetime import datetime\n" +
+                        "\n" +
+                        "\n" +
+                        "# create path name\n" +
+                        "path = \"Codes\" \n" +
+                        "\n" +
+                        "# create dataset dir\n" +
+                        "if not os.path.exists(path):\n" +
+                        "    os.makedirs(path)\n" +
+                        "\n" +
+                        "# go to dir\n" +
+                        "os.chdir(path)\n" +
+                        "\n" +
+                        "# acquire repositories\n" +
+                        "with open(\"filename\",'r') as repo_list:\n" +
+                        "    for count, repo in enumerate(repo_list):    \n" +
+                        "       pair_id = \"LBYCPD2_Pair\" + str(count)\n" +
+                        "       print(\"Downloading \", pair_id)\n" +
+                        "       Repo.clone_from(repo.strip(), pair_id, branch='master')\n" +
+                        "   \n" +
+                        "print(\"---- COMPLETED ----\")";
+                String name=repo.getAbsolutePath().replaceAll("\\\\","\\\\\\\\");
+                prg=prg.replace("filename",name);
+                System.out.println(prg);
+                BufferedWriter out = new BufferedWriter(new FileWriter("RepoGrabber.py"));
+                out.write(prg);
+                out.close();
         } catch (Exception e) {
             System.out.println(e);
         }
