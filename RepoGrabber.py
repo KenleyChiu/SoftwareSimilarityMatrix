fdecbb35a058b14c7de2814a6f796da4c092ddf4
Keyaru <kenley_chiu@dlsu.edu.ph>
2020-02-22 12:18:07 +0800

    Changes

--- RepoGrabber.py
+++ RepoGrabber.py
@@ -14,10 +14,9 @@ if not os.path.exists(path):
 os.chdir(path)
 
 # acquire repositories
-<<<<<<< HEAD
 with open("D:\\Downloads\\College stuff\\SoftwareSimilarityMatrix\\Repo.txt",'r') as repo_list:
     for count, repo in enumerate(repo_list):    
-       pair_id = "Pair " + str(count+1)
+       pair_id = "LBYCPD2_Pair" + str(count)
        print("Downloading ", pair_id)
        Repo.clone_from(repo.strip(), pair_id, branch='master')
    
